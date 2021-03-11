aa696487c4861e4cb4d96b9e5d42dae9245743f5
Malin Henningsson <malin.henningsson@jayway.com>
2021-03-11 15:54:54 +0100

    changes direction on L and R letters in string

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 function App() {
   const [userInput, setUserInput] = useState("");
   const [roomSize, setRoomSize] = useState({ x: 0, y: 0 });
-  const [startPosition, setStartPosition] = useState([0, 0, "N"]);
-  const [endPosition, setEndPosition] = useState([]);
-  const directions = ["E", "W", "N", "S"];
+  const [startPosition, setStartPosition] = useState({ x: 0, y: 0, d: "N" });
+  const [endPosition, setEndPosition] = useState({});
+  const directions = ["N", "E", "S", "W"];
 
   const getStartValues = () => {
     // Get random sized room
@@ -21,7 +21,12 @@ function App() {
     const robotPositionY = Math.floor(Math.random() * y);
     const robotDirection =
       directions[Math.floor(Math.random() * directions.length)];
-    setStartPosition([robotPositionX, robotPositionY, robotDirection]);
+
+    setStartPosition({
+      x: robotPositionX,
+      y: robotPositionY,
+      d: robotDirection,
+    });
   };
 
   // On first render, set random value for room size and start position
@@ -29,15 +34,60 @@ function App() {
     getStartValues();
   }, []);
 
+  const getCardinalDirection = (movement, prevDir) => {
+    let newDir = "";
+    if (movement === "L") {
+      if (prevDir === "N") {
+        newDir = "W";
+      } else if (prevDir === "E") {
+        newDir = "N";
+      } else if (prevDir === "S") {
+        newDir = "E";
+      } else if (prevDir === "W") {
+        newDir = "S";
+      }
+    } else if (movement === "R") {
+      if (prevDir === "N") {
+        newDir = "E";
+      } else if (prevDir === "E") {
+        newDir = "S";
+      } else if (prevDir === "S") {
+        newDir = "W";
+      } else if (prevDir === "W") {
+        newDir = "N";
+      }
+    }
+    return newDir;
+  };
+
   const moveRobot = (userInput) => {
+    let newPosition = JSON.parse(JSON.stringify(startPosition));
+    const input = userInput.trim();
+
+    for (let i = 0; i < input.length; i++) {
+      if (input.charAt(i).toUpperCase() === "L") {
+        // Change direction
+        const newDir = getCardinalDirection("L", newPosition.d);
+        newPosition.d = newDir;
+      }
+      if (input.charAt(i).toUpperCase() === "R") {
+        // Change direction
+        const newDir = getCardinalDirection("R", newPosition.d);
+        newPosition.d = newDir;
+      }
+      if (input.charAt(i).toUpperCase() === "F") {
+        // Change position
+      }
+    }
+    setEndPosition(newPosition);
     setUserInput("");
   };
 
   const restartTask = () => {
     setUserInput("");
-    setEndPosition([]);
-    setStartPosition([]);
     setRoomSize({ x: "", y: "" });
+    setStartPosition({ x: 0, y: 0, d: "N" });
+    setEndPosition({});
     getStartValues();
   };
 
@@ -87,12 +137,17 @@ function App() {
           room.
         </p>
         <p>
-          Start position: <strong>{startPosition.join(", ")}</strong>
+          Start position:{" "}
+          <strong>
+            {startPosition.x}, {startPosition.y}, {startPosition.d}
+          </strong>
         </p>
         <p>
           End position:{" "}
-          {endPosition.length > 0 ? (
-            <strong>{endPosition.join(", ")}</strong>
+          {endPosition.x ? (
+            <strong>
+              {endPosition.x}, {endPosition.y}, {endPosition.d}
+            </strong>
           ) : (
             <em>please enter robot movements above to get an end position</em>
           )}
